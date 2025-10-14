0ae69b2a2e88d8892140fd70ef41bb1f8a245b1f
CONNOR LEE <bcl9779@nyu.edu>
2025-10-14 14:39:10 -0400

    flask stuff

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template
+import pymongo
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+def create_app():
+    app = Flask(__name__)
+    load_dotenv()
+
+    # client =
+
+    @app.route("/")
+    def hello_world():
+        return render_template('index.html')
+        
+    @app.route("/about")
+    def about():
+        return render_template('base.html')
+
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    FLASK_PORT = os.getenv("FLASK_PORT", 5000)
+    FLASK_ENV = os.getenv("FLASK_ENV")
+    
+    print(f"FLASK_ENV: {FLASK_ENV}, FLASK_PORT: {FLASK_PORT}")
+
+    app.run(port=FLASK_PORT)
